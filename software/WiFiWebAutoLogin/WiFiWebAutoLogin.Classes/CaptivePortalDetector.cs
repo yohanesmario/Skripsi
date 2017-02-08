34b32eda5b4e51c23652cee563e62d2985231737
Yohanes Mario Chandra <yohanes.mc@gmail.com>
2017-02-08 11:23:01 +0700

    NetworkChange Event Handler

--- software/WiFiWebAutoLogin/WiFiWebAutoLogin.Classes/CaptivePortalDetector.cs
+++ software/WiFiWebAutoLogin/WiFiWebAutoLogin.Classes/CaptivePortalDetector.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
 using Windows.Foundation.Metadata;
+using Windows.Networking.Connectivity;
 using Windows.Storage;
 using Windows.UI.Xaml.Controls;
 
@@ -25,7 +27,7 @@ namespace WiFiWebAutoLogin {
         public async Task setup(WebView webView) {
             this.webView = webView;
 
-            this.webView.Navigate(new Uri(Conf.uri));
+            this.startNetworkProbing();
         }
 
         public WebView getWebView() {
@@ -44,11 +46,57 @@ namespace WiFiWebAutoLogin {
             StorageFolder InstallationFolder = Windows.ApplicationModel.Package.Current.InstalledLocation;
             StorageFile file = await InstallationFolder.GetFileAsync(@"JavaScript\DeployListeners.js");
             string js = await FileIO.ReadTextAsync(file);
-            await this.webView.InvokeScriptAsync("eval", new string[] { js });
+            //await this.webView.InvokeScriptAsync("eval", new string[] { js });
         }
 
         public async void notify(string args) {
             await this.webView.InvokeScriptAsync("eval", new string[] { "document.body.innerHTML = '" + args + "';" });
+        }
+
+        private string getSSID() {
+            ConnectionProfile connectionProfile = NetworkInformation.GetInternetConnectionProfile();
+            string data = "";
+            if (connectionProfile != null) {
+                IEnumerable<string> enumerable = connectionProfile.GetNetworkNames().AsEnumerable();
+                foreach (string v in enumerable) {
+                    if (data.Equals("")) {
+                        data += v;
+                    }
+                    else {
+                        data += " | " + v;
+                    }
+                }
+                if (data.Equals("")) {
+                    return null;
+                }
+                else {
+                    return data;
+                }
+            }
+            else {
+                return null;
+            }
+        }
+
+        private void startNetworkProbing() {
+            NetworkChange.NetworkAddressChanged += new NetworkAddressChangedEventHandler(NetworkEventHandler);
+            this.onNetworkChange();
+        }
+
+        private async void NetworkEventHandler(object sender, EventArgs e) {
+            await Windows.ApplicationModel.Core.CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () => {
+                this.onNetworkChange();
+            });
+        }
+
+        private void onNetworkChange() {
+            string ssid = this.getSSID();
+            if (ssid == null) {
+                this.webView.NavigateToString("<br /><br /><br />NO CONNECTION");
+            }
+            else {
+                this.webView.NavigateToString("<br /><br /><br />" + ssid);
+            }
         }
     }
 }
