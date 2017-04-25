916d15f1e974b893e64c40ed3e0c8d410f72a3d0
Yohanes Mario Chandra <yohanes.mc@gmail.com>
2017-04-25 17:16:51 +0700

    revisi 20/04/17

--- software/WiFiWebAutoLogin/WiFiWebAutoLogin.RuntimeComponents/Properties/AssemblyInfo.cs
+++ software/WiFiWebAutoLogin/WiFiWebAutoLogin.RuntimeComponents/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WiFiWebAutoLogin.Connector")]
+[assembly: AssemblyTitle("WiFiWebAutoLogin.RuntimeComponents")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WiFiWebAutoLogin.Connector")]
+[assembly: AssemblyProduct("WiFiWebAutoLogin.RuntimeComponents")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
